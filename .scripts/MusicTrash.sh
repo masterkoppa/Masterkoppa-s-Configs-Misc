3e86a72040ebad99885cdc50af17ec6570dc2fb3
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2013-05-15 17:57:50 -0400

    Add a new script folder and a script to detect trash files in my music folder
    This script will be usefull when I clean things up in my collection
    hopefully some time soon....

---
+++ .scripts/MusicTrash.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+du -a --exclude="*.mp3" --exclude="*.flac" --exclude="*.acc" --exclude="*.wma" --exclude="*.MP3" --exclude="*.m4a" --exclude="*.jpg" --exclude="*.png" --exclude="*.bmp" --exclude="*.avi" -h ~/Music/
